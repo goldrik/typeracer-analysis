550e47631a161b4dbd1b4678c81a7514536681be
Aurik Sarker <auriksarker@gmail.com>
2025-05-06 02:27:12 -0400

    Finalized typingLog parsing

--- script_typinglog.py
+++ script_typinglog.py
@@ -5,164 +5,6 @@ import re
 #%%
 
 
-def tl1_to_char_ms(tl:str, text:str):
-    # Split by |
-    T = tl[typinglog_pipe(tl)+1:]
-    # Do this to make regex better
-    T = ',' + T[:-1]
-
-    # Split into words
-    wordnums_pattern = r',(\d+),(\d+),'
-    wordnums_pattern_ = wordnums_pattern.replace('(', '').replace(')', '')
-    # The {ms} portion is easy to handle
-    # Find those using regex and split the string
-    ms_pattern = r',-?\d+,'
-    char_pattern = r'(\d+)([+\-$])(\\"|.)'
-
-    # FORMAT
-    # ,{ms},{ind+char}+,
-    
-    ww0 = re.findall(wordnums_pattern, T)
-    ww1 = re.split(wordnums_pattern_, T)[1:]
-
-    ms_all = []
-    word_inds = []
-    ops = []
-    chars = []
-
-    word_nums = []
-    pd_inds = []
-
-    word_strokes = []
-
-    words = text.split(' ')
-    assert(len(words) == len(ww0))
-
-    i = -1
-    for w in range(len(ww0)):
-        W = ',' + ww1[w]
-        tt0 = re.findall(ms_pattern, W)
-        tt1 = re.split(ms_pattern, W)[1:]
-
-        word_strokes.append(int(ww0[w][1]))
-        for t0, t1 in zip(tt0, tt1):
-            ms = int(t0[1:-1])
-
-            strs = re.findall(char_pattern, t1)
-            if len(strs) != 0:
-                i += 1
-            for word_ind, op, char in strs:
-                ms_all.append(ms)
-
-                word_ind_ = int(word_ind)
-                c = char[-1]
-                word_inds.append(word_ind_)
-                ops.append(op)
-                chars.append(c)
-
-                pd_inds.append(i)
-                word_nums.append(w)
-
-                ms = 0
-
-
-    TL = pd.DataFrame({'word':word_nums, 'ind':word_inds, 'char':chars, 'ms':ms_all, 'op':ops}, index=pd_inds)
-
-    return TL, word_strokes
-
-def tl_df_to_words(TL, text):
-    T = len(TL)
-
-
-    words=  text.split(' ')
-    words[:-1] = [w+' ' for w in words[:-1]]
-
-    chars_text = []
-    chars_ms = []
-    mistakes = []
-    chars_typed = []
-
-    t = ''
-    word = []
-    w = 0
-    w_i = 0
-    ms_ = 0
-    mistake = False
-    mistake_char = ''
-    for i in range(T):
-        c = TL.iloc[i]['char']
-        word_ind = TL.iloc[i]['ind']
-        op = TL.iloc[i]['op']
-
-        if op == '+':
-            word.append(c)
-            assert(word[word_ind] == c)
-
-        elif op == '-':
-            assert(word[word_ind] == c)
-            word.pop(word_ind)
-
-        elif op == '$':
-            word[word_ind] = c
-            assert(word[word_ind] == c)
-
-        # Check each time the word reaches correct status
-        if ''.join(word) == words[w][:w_i+1]:
-        # if word == words[w][:len(word)]:
-            c_ = words[w][w_i]
-            
-            chars_text.append(c_)
-            ms_ += TL.iloc[i]['ms']
-            chars_ms.append(ms_)
-            mistakes.append(mistake)
-            if not mistake: 
-                chars_typed.append(c_)
-            else:
-                chars_typed.append(mistake_char)
-
-            w_i += 1
-            ms_ = 0
-            mistake = False
-            mistake_char = ''
-        else:
-            mistake = True
-            if mistake_char == '':
-                mistake_char = c
-            ms_ += TL.iloc[i]['ms']
-
-
-        if (i == T-1) or ((c == ' ') and (TL.iloc[i+1]['ind'] == 0) and (TL.iloc[i+1]['op'] == '+')):
-            t += ''.join(word)
-            word = []
-
-            w += 1
-            w_i = 0
-            ms_ = 0
-            mistake = False
-            mistake_char = ''
-
-    C = pd.DataFrame({'char':chars_text, 'ms':chars_ms, 'mistake':mistakes, 'typed':chars_typed})
-
-    words_ms = []
-    word_lens = []
-    word_strokes = []
-
-    word_inds = TL['word'].to_numpy()
-    assert(len(np.unique(word_inds)) == len(words))
-    for i in range(len(words)):
-        inds = word_inds == i
-        words_ms.append(TL['ms'][inds].sum())
-        word_lens.append(len(words[i]))
-
-        inds = TL[inds].index
-        word_strokes.append(inds.max() - inds.min() + 1)
-
-    W = pd.DataFrame({'word':words, 'ms':words_ms, 'len':word_lens, 'strokes':word_strokes})
-
-
-    return t, C, W
-
-
 def tl_df_to_text(TL):
     T = len(TL)
     t = ''
@@ -217,10 +59,12 @@ for race in inds:
     text = texts.loc[textID, 'Text']
     wpm = races.loc[race, 'WPM']
     acc = races.loc[race, 'Accuracy']
-    # numwords = texts.loc[textID, 'NumWords']
 
-    chars_total = len(text)
-    words_total = len(text.split(' '))
+    chars_total = texts.loc[textID, 'NumChars']
+    words_total = texts.loc[textID, 'NumWords']
+
+    # chars_total = len(text)
+    # words_total = len(text.split(' '))
 
     assert(tl.count('|') == 1)
     assert(tl[typinglog_pipe(tl)+1] == '0')
@@ -247,14 +91,10 @@ for race in inds:
 
     assert(TL1['WordInd'].max()+1 == words_total)
     assert(sum(word_strokes) == TL1.index[-1]+1)
-
-    # text_ = tl_df_to_text(TL1)
-    # text_, C, W = tl_df_to_words(TL1, text)
     
 
     assert(TL1['Ms'].sum() == C['Ms'].sum())
     assert(TL1['Ms'].sum() == W['Ms'].sum()) 
-
 
 
     assert(text_ == text)
@@ -262,8 +102,12 @@ for race in inds:
     assert(''.join(W['Word']) == text)
 
     acc_ = 100 * (1 - C['Mistake'].sum() / len(C))
-    # print(f'{acc_:.2f}\t{acc:.2f}')
+    inds_chars = (C['Char'] != ' ').to_numpy()
+    acc__ = 100 * (1 - C.loc[inds_chars, 'Mistake'].sum() / np.count_nonzero(inds_chars))
+    print(f'{acc:.2f}\t{acc_:.2f}\t{acc__:.2f}')
 
     assert(all(np.array(word_strokes) == W['Keystrokes'].to_numpy()))
 
+    mistake_words = W['Mistake'].to_numpy()
+    assert(all(W.loc[~mistake_words, 'Word'] == W.loc[~mistake_words, 'Attempt']))
 
--- typing_analysis.py
+++ typing_analysis.py
@@ -20,8 +20,6 @@ def text_to_sections(text: str, numSections: int=8) -> list[str]:
     # Exact characters where the text may be divided into sections
     inds = (np.arange(numSections+1) * section_len_chars).astype(int)
 
-    # text = text[:-1]
-
     ind_end = 0
     sections = []
     for i in range(numSections):
@@ -65,7 +63,202 @@ def typinglog_pipe(tl: str) -> int:
     return pipe_occurence[-1].start()
 
 
-# Parse the first half of typinglog
+# Outputs three dataframes: all keystrokes, text characters, words
+def parse_typinglog(tl, text):
+    # This gives us all the keystrokes/entries first
+    TL = parse_typinglog_complete(tl)
+    num_entries = len(TL)
+
+    words=  text.split(' ')
+    words[:-1] = [w+' ' for w in words[:-1]]
+
+    ## CHARACTERS
+
+    # For each character in the text
+    chars_text = []
+    word_inds = []
+    # Time it took to type each character CORRECTLY
+    # i.e. includes time for incorrect keystrokes
+    chars_ms = []
+    # Was there a mistake made when typing this character
+    mistakes = []
+    # Which character did the user type
+    chars_typed = []
+
+    def reset_word_vars():
+        # char_ms, mistake, first_typed_char
+        return 0, False, ''
+
+
+    text_ = ''
+    word_ = []
+    word_ind = 0
+    char_ind = 0
+    char_ms, mistake, first_typed_char = reset_word_vars()
+    for i in range(num_entries):
+        c = TL.iloc[i]['Char']
+        char_ind_ = TL.iloc[i]['CharInd']
+        op = TL.iloc[i]['Op']
+
+        if op == '+':
+            word_.append(c)
+            assert(word_[char_ind_] == c)
+
+        elif op == '-':
+            assert(word_[char_ind_] == c)
+            word_.pop(char_ind_)
+
+        elif op == '$':
+            word_[char_ind_] = c
+            assert(word_[char_ind_] == c)
+
+        # Accumulate the time needed to type this character
+        char_ms += TL.iloc[i]['Ms']
+
+        # For each character in the word, use the word's substring (so far)
+        #   check if the user has typed the substring correctly yet
+        substring = words[word_ind][:char_ind+1]
+        if ''.join(word_) == substring:
+            # The correct character
+            c_ = words[word_ind][char_ind]
+            chars_text.append(c_)
+            word_inds.append(word_ind)
+
+            chars_ms.append(char_ms)
+            mistakes.append(mistake)
+            if not mistake: 
+                # Normally, the correct character was typed
+                chars_typed.append(c_)
+            else:
+                chars_typed.append(first_typed_char)
+
+            char_ind += 1
+            char_ms, mistake, first_typed_char = reset_word_vars()
+        else:
+            mistake = True
+            # Set this variable for just the first character
+            if first_typed_char == '':
+                first_typed_char = c
+
+        # If we've reached the end of the text or end of word
+        if (i == num_entries-1) or \
+            ((c == ' ') and (TL.iloc[i+1]['CharInd'] == 0) and (TL.iloc[i+1]['Op'] == '+')):
+            text_ += ''.join(word_)
+            word_ = []
+
+            word_ind += 1
+            char_ind = 0
+            char_ms, mistake, first_typed_char = reset_word_vars()
+
+    C = pd.DataFrame({'WordInd':word_inds, 'Char':chars_text, 'Ms':chars_ms, 'Mistake':mistakes, 'Typed':chars_typed})
+
+    ## WORDS
+
+    words_ms = []
+    # The full attempt for this word
+    word_keystrokes = []
+    word_mistakes = []
+    word_lens = []
+    # Number of keystrokes needed (not entries)
+    word_strokes = []
+
+    word_inds_0 = TL['WordInd'].to_numpy()
+    word_inds_1 = C['WordInd'].to_numpy()
+
+    ops = TL['Op'].to_numpy()
+    inds_add = (ops == '+') | (ops == '$')
+    
+    assert(len(np.unique(word_inds)) == len(words))
+    for i in range(len(words)):
+        inds_0 = word_inds_0 == i
+        inds_1 = word_inds_1 == i
+        words_ms.append(TL['Ms'][inds_0].sum())
+
+        word_keystrokes.append(''.join(TL['Char'][inds_0 & inds_add]))
+        word_mistakes.append(C['Mistake'][inds_1].any())
+
+        word_lens.append(len(words[i]))
+
+        inds = TL[inds_0].index
+        word_strokes.append(inds.max() - inds.min() + 1)
+
+    W = pd.DataFrame({'Word':words, 'Attempt':word_keystrokes, 'Ms':words_ms, 'Mistake':word_mistakes, 'Length':word_lens, 'Keystrokes':word_strokes})
+
+    return TL, C, W, text_
+
+
+# Parse the second half of typingLog
+#   This contains *all* the keystrokes
+def parse_typinglog_complete(tl:str):
+    # Split by |
+    T = tl[typinglog_pipe(tl)+1:]
+    # Do this to make regex better
+    T = ',' + T[:-1]
+
+    # Split into words
+    wordnums_pattern = r',(\d+),(\d+),'
+    # This makes it so regex matches are all unique (no overlaps)
+    wordnums_pattern_ = wordnums_pattern.replace('(', '').replace(')', '')
+    
+    # Each word now comprises of multiple keystrokes
+    # Format for each entry: 
+    #   ,{ms},{ind}{op}{char},
+    ms_pattern = r',-?\d+,'
+    char_pattern = r'(\d+)([+\-$])(\\"|.)'
+
+    word_attempts = re.split(wordnums_pattern_, T)[1:]
+
+    # Arrays for DataFrame, values for each entry
+    times = []
+    char_inds = []
+    ops = []
+    chars = []
+
+    # Which word
+    word_inds = []
+    # DataFrame index is for each typingLog keystroke (can have duplicates)
+    #   ** a keystroke may have multiple "entries" (i.e. selection -> replace)
+    pd_inds = []
+
+    # NOTE: 
+    # Text has multiple words -> the user attempts the word -> the attempt is comprised of multiple keystrokes 
+    #   -> an keystroke is usually just one typingLog entry, but can be multiple (if select & replace)
+
+    i = -1
+    # Loop through each full word (i.e. all keystrokes for that attempt)
+    for w,W in enumerate(word_attempts):
+        W_ = ',' + W
+        ms_vals = re.findall(ms_pattern, W_)
+        keystrokes = re.split(ms_pattern, W_)[1:]
+
+        for ms_str, keystroke in zip(ms_vals, keystrokes):
+            ms = int(ms_str[1:-1])
+
+            entries = re.findall(char_pattern, keystroke)
+            # Iterate the typingLog keystroke (dataframe index)
+            i += len(entries) != 0
+            assert(len(entries) > 0)
+
+            for char_ind, op, char in entries:
+                times.append(ms)
+
+                char_inds.append(int(char_ind))
+                ops.append(op)
+                
+                # The "character" found with regex can be \" (two characters), so take the last index
+                chars.append(char[-1])
+
+                pd_inds.append(i)
+                word_inds.append(w)
+
+                # Only save the ms once (to avoid double-dipping)
+                ms = 0
+
+    TL = pd.DataFrame({'WordInd':word_inds, 'CharInd':char_inds, 'Char':chars, 'Ms':times, 'Op':ops}, index=pd_inds)
+    return TL
+
+
+# Parse the first half of typingLog
 # This contains all the correct characters and the time it took to *correctly* type each
 #   * not all keystrokes, only correct ones shown
 def parse_typinglog_simple(tl:str) -> pd.DataFrame:
@@ -107,7 +300,29 @@ def parse_typinglog_simple(tl:str) -> pd.DataFrame:
                 lookingForMs = True
         
     ms = [int(m) for m in ms]
-    TL = pd.DataFrame({'char':chars, 'ms':ms})
+    TL = pd.DataFrame({'Char':chars, 'Ms':ms})
 
     return TL
 
+
+# The typingLog contains two values for each word
+#   its starting index in the full text
+#   the number of keystrokes the user took to type the word
+# This function is not used to perform parsing (above), but can be used for validation
+def parse_typinglog_wordvals(tl:str):
+    # Split by |
+    T = tl[typinglog_pipe(tl)+1:]
+    # Do this to make regex better
+    T = ',' + T[:-1]
+
+    # Split into words
+    wordnums_pattern = r',(\d+),(\d+),'
+    wordnums = re.findall(wordnums_pattern, T)
+
+    text_inds, num_strokes = zip(*wordnums)
+
+    text_inds = [int(i) for i in text_inds]
+    num_strokes = [int(i) for i in num_strokes]
+
+    return text_inds, num_strokes
+    
