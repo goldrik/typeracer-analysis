be93b993cd07989a7fecaa1e9f371144eb8d0b85
Aurik Sarker <auriksarker@gmail.com>
2025-05-09 01:19:09 -0400

    Revamped script for testing typingLog parsing, added more asserts

--- script_typinglog.py
+++ script_typinglog.py
@@ -4,6 +4,7 @@
 #%%
 #!%load_ext autoreload
 #!%autoreload 2
+#!%load_ext line_profiler
 
 import numpy as np
 import pandas as pd
@@ -19,6 +20,7 @@ from parse_soup import *
 from typeracer_utils import *
 # from typing_analysis import *
 from typing_analysis_ import *
+# from typing_analysis_ import typinglog_pipe, parse_typinglog, parse_typinglog_complete, reconstruct_text_typinglog
 
 
 #%%
@@ -76,23 +78,35 @@ ind = 2742
 
 ind = 2732
 
+ind = 2480
+
+ind = 2407
+# ind = 2382
+
+ind = 821
+
 
 #%%
 
 inds = races.index
-# inds = range(2163-1, 0, -1)
-# inds = range(races.index.max(), 2742, -1)
 # inds = np.random.choice(races.index, 300, replace=False)
+# inds = range(2500, 0, -1)
+# inds = range(races.index.max(), 2742, -1)
 # inds = [ind]
-# inds = [7548]
+inds = [2]
 
+T_D = []
+MT = []
+
+st = time()
 for race in inds:
     print(race)
 
     textID = races.loc[race, 'TextID']
 
     tl = races.loc[race, 'TypingLog']
-    text = texts.loc[textID, 'Text']
+    # tl = races.loc[race, 'TypingLog']
+    text = races.loc[race, 'TypedText']
     wpm = races.loc[race, 'WPM']
     acc = races.loc[race, 'Accuracy']
 
@@ -107,113 +121,62 @@ for race in inds:
     ####################
 
     TL0 = parse_typinglog_simple(tl)
-    # assert(''.join(TL0['Char']) == text)
-    # if ''.join(TL0['Char']) != text:
-    #     print(textID)
-    #     print(text)
-    #     print(''.join(TL0['Char']))
-    #     continue
+    # TL = parse_typinglog_complete(tl)[0]
+    # text_, tp = reconstruct_text_typinglog(TL)
 
-    ####################
 
-    TL = parse_typinglog_complete(tl)[0]
-
-    lc, fo = [], []
-    for _,W in TL.groupby('Window'):
-        lc.append(W.iloc[-1]['Char'])
-        fo.append(W.iloc[0]['Op'])
-    lc = np.array(lc)
-    fo = np.array(fo)
-
+    # lc, fo = [], []
+    # for _,W in TL.groupby('Window'):
+    #     lc.append(W.iloc[-1]['Char'])
+    #     fo.append(W.iloc[0]['Op'])
+    # lc = np.array(lc)
+    # fo = np.array(fo)
     # if ~np.all(lc[:-1] == ' '):
     #     print('\tThere was a window that didnt end with space')
-    if ~np.all(fo == '+'):
-        print('\tThere was a window that didnt start with an addition')
+    # if ~np.all(fo == '+'):
+    #     print('\tThere was a window that didnt start with an addition')
 
-    text_ = reconstruct_text_typinglog(TL)
+    # a = time()
+    TL, C, W, text_  = \
+        parse_typinglog(tl)
+    # print(f'\t{ (time()-a)*1e3:0.2f} ms')
+
+
+
+    # assert(len(tp)-1 == TL['Stroke'].iloc[-1])
     assert(text_ == text)
 
-
-#%%
-
-raise Exception
-
-inds = races.index
-# inds = range(2800, 0, -1)
-# inds = np.random.choice(races.index, 300, replace=False)
-# inds = [ind]
-# inds = [7538]
-
-for race in inds:
-    # if race in [2751, 2742]:
-    #     continue
-    print(race)
-
-    textID = races.loc[race, 'TextID']
-
-    tl = races.loc[race, 'TypingLog']
-    text = texts.loc[textID, 'Text']
-    wpm = races.loc[race, 'WPM']
-    acc = races.loc[race, 'Accuracy']
-
-    chars_total = texts.loc[textID, 'NumChars']
-    words_total = texts.loc[textID, 'NumWords']
-
-    # chars_total = len(text)
-    # words_total = len(text.split(' '))
-
-    assert(tl.count('|') == 1)
-    assert(tl[typinglog_pipe(tl)+1] == '0')
-
-    TL0 = parse_typinglog_simple(tl)
-    assert(''.join(TL0['Char']) == text)
-
-
-    # TL1, _ = tl1_to_char_ms(tl, text)
-    TL1 = parse_typinglog_complete(tl)
-    TL1, C,W, text_ = parse_typinglog(tl, text)
-    _, word_strokes = parse_typinglog_wordvals(tl)
-
-
-    # print_tl_df(TL1)
     
-
-    wpm0 = (chars_total / 5) / (TL0['Ms'].sum() / 1e3 / 60)
-    wpm1 = (chars_total / 5) / (TL1['Ms'].sum() / 1e3 / 60)
-    assert(wpm0 == wpm1)
-
-    # print(f'{wpm} {wpm1:.2f}')
-
-    assert(TL1['WordInd'].max()+1 == words_total)
-    assert(sum(word_strokes) == TL1.index[-1]+1)
-    
-    assert(text_ == text)
     assert(''.join(C['Char']) == text)
     assert(''.join(W['Word']) == text)
+    
+    assert(TL0['Ms'].sum() == TL['Ms'].sum())
+    assert(TL['Ms'].sum() == C['Ms'].sum())
+    assert(TL['Ms'].sum() == W['Ms'].sum())
 
-    assert(TL1['Ms'].sum() == C['Ms'].sum())
-    assert(TL1['Ms'].sum() == W['Ms'].sum()) 
+    nw = len(text.split(' '))
+    assert(nw == C['Word'].iloc[-1]+1)
+    assert(nw == len(W))
 
-    # acc_ = 100 * (1 - C['Mistake'].sum() / len(C))
-    # inds_chars = (C['Char'] != ' ').to_numpy()
-    # acc__ = 100 * (1 - C.loc[inds_chars, 'Mistake'].sum() / np.count_nonzero(inds_chars))
-    # print(f'{acc:.2f}\t{acc_:.2f}\t{acc__:.2f}')
+    # for wa,WA in zip([wpm,acc], ['wpm', 'acc']):
+    #     wa_, opt_t, opt_m = compute_wpm_acc_best(C, wa, WA)
+    #     print(f'\t{wa:0.2f}\t{wa_:0.2f}\t{opt_t:<12}\t{opt_m}')
 
-    # assert(all(np.array(word_strokes) == W['Keystrokes'].to_numpy()))
-    if len(word_strokes) != len(W) or \
-        not all(np.array(word_strokes) == W['Keystrokes'].to_numpy()):
-        print('\tWord Strokes Issue')
+    t_d = np.count_nonzero(C['Ms'] - TL0['Ms'])
+    if t_d:
+        T_D.append([race, t_d])
+        print("\tTime Difference between TL0 and C:", t_d)
+        print('\t', np.where(C['Ms'] - TL0['Ms'])[0])
 
-    mistake_words = W['Mistake'].to_numpy()
-    assert(all(W.loc[~mistake_words, 'Word'] == W.loc[~mistake_words, 'Attempt']))
+    C_ = C[~C['Mistake']]
+    assert((C_['Char'] == C_['Typed']).all())
+    C_ = C[C['Mistake']]
+    mt = (C_['Char'] == C_['Typed']).sum()
+    if mt:
+        MT.append([race, mt])
+        print("\tMistake Char has correct Typed Char:", mt)
 
 
-    a = []
-    ops = TL1['Op'].to_numpy()
-    charinds = TL1['CharInd'].to_numpy()
-    for i in range(len(TL1)-1):
-        if (ops[i] == '+') and (ops[i+1] == '+'):
-            if charinds[i+1] < charinds[i]:
-                a.append(charinds[i+1]) 
+print(f'\t{ (time()-st):0.2f} secs')
+print(len(T_D), len(MT))
 
-    assert(not any(a))
