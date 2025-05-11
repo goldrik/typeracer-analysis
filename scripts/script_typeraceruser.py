3bf116f25d07949971cf5ae80cd14f7c17aeea71
Aurik Sarker <auriksarker@gmail.com>
2025-05-11 02:37:39 -0400

    Utilize saving / loading feature, simply

--- scripts/script_typeraceruser.py
+++ scripts/script_typeraceruser.py
@@ -18,6 +18,8 @@ from time import time, sleep
 from parse_soup import *
 from typeracer_utils import *
 from typing_analysis import *
+from url_formatstrings import *
+from TypeRacerUser import TypeRacerUser
 
 
 #%%
@@ -26,25 +28,27 @@ dotenv.load_dotenv()
 USER: str = os.getenv('USER')
 FH_PKL: str = os.getenv('FH_PKL')
 
-
-#%%
-## VARS
-# URL format strings
-from url_formatstrings import url_base, url_user, url_races, url_race, url_text
-
+FN_PKL_USER = os.path.join(FH_PKL, f'typeracer_{USER}.pkl')
 FN_PKL_HTMLS = os.path.join(FH_PKL, f'typeracer_htmls.pkl')
 
 #%%
-## OBJECT
-from TypeRacerUser import TypeRacerUser
-
-user = TypeRacerUser('goldrik', fn_htmls=FN_PKL_HTMLS)
-# user.update()
-user.update(10)
+## LOAD
+try: 
+    userdata = TypeRacerUser.load(FN_PKL_USER)
+except:
+    userdata = TypeRacerUser('goldrik', fn_htmls=FN_PKL_HTMLS)
 
 
 #%%
-# user.populate_races(10)
+## LOAD
+# Make sure it has the htmls loaded
+userdata.load_htmls()
+userdata.update()
+# userdata.update(10)
 
-user.populate_results()
-user.results
+userdata.populate_results()
+
+
+#%%
+## SAVE
+userdata.save(FN_PKL_USER)
